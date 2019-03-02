3ef25af929046fb9c329c341d8deb739e5874e5b
Dinu Sergiu-Andrei <dinusergiuandrei@yahoo.com>
2019-03-02 22:01:04 +0200

    Added readme.

--- main.py
+++ main.py
@@ -188,20 +188,3 @@ if __name__ == '__main__':
     print(t[3])  # easy read
 
 
-# 15 GAMES FOR EACH PAIRING
-# [[ 0.  3.  0. -6.]
-#  [-3.  0.  0. -4.]
-#  [ 0.  0.  0.  4.]
-#  [ 6.  4. -4.  0.]]
-# {'Random player': -3.0, 'Copy player': -7.0, 'Genetic algorithm': 4.0, 'Artificial neuronal network': 6.0}
-
-# [[   0.  -32.   12.    3.]
-#  [  32.    0.  168. -427.]
-#  [ -12. -168.    0.  -11.]
-#  [  -3.  427.   11.    0.]]
-# {'Random player': -17.0, 'Copy player': -227.0, 'Genetic algorithm': -191.0, 'Artificial neuronal network': 435.0}
-# [[   0.   -4.    2.    6.]
-#  [   4.    0.   79. -143.]
-#  [  -2.  -79.    0.   -5.]
-#  [  -6.  143.    5.    0.]]
-# {'Random player': 4.0, 'Copy player': -60.0, 'Genetic algorithm': -86.0, 'Artificial neuronal network': 142.0}
